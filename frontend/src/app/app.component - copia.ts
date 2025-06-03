43926881692fa86d87dc4db8ca73129a4bbd0575
Ruben s002411 <ruben.burcion-gil@renault.com>
2025-06-03 08:01:54 +0200

    Arreglado app.module.ts y estructura de componentes

---
+++ frontend/src/app/app.component - copia.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent implements OnInit {
+  currentSlide = 0;
+  sliderImages = [
+    'assets/slide1.jpg',
+    'assets/slide2.jpg',
+    'assets/slide3.jpg',
+  ];
+
+  sponsors = [
+    'assets/sponsor1.png',
+    'assets/sponsor2.png',
+    'assets/sponsor3.png',
+    'assets/sponsor4.png',
+  ];
+
+  noticias = [
+    { imagen: 'assets/news1.jpg', texto: 'Victoria del equipo en casa' },
+    { imagen: 'assets/news2.jpg', texto: 'Nueva equipación disponible' },
+    { imagen: 'assets/news3.jpg', texto: 'Calendario de entrenamientos' },
+    { imagen: 'assets/news4.jpg', texto: 'Entrevista al capitán' },
+    { imagen: 'assets/news5.jpg', texto: 'Torneo local próximo domingo' },
+    { imagen: 'assets/news6.jpg', texto: 'Ampliación del campo de juego' },
+  ];
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.nextSlide();
+    }, 5000); // Cambia cada 5s
+  }
+
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.sliderImages.length;
+  }
+
+  prevSlide() {
+    this.currentSlide =
+      (this.currentSlide - 1 + this.sliderImages.length) %
+      this.sliderImages.length;
+  }
+}
